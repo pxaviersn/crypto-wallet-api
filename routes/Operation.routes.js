aa23ff1366a68a39771a67386d91944dd701d3da
pxaviersn <pauloxaviersn@live.com>
2023-03-18 10:41:17 -0700

    Fixing import capitalization

--- routes/Operation.routes.js
+++ routes/Operation.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import Operation from '../models/operation.model.js';
+import Operation from '../models/Operation.model.js';
 import UserModel from '../models/user.model.js';
 import isAuthenticatedMiddleware from '../middlewares/isAuthenticatedMiddleware.js'
 import 'dotenv/config'
